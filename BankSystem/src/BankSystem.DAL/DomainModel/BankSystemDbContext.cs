07d45b7b045123c6e69971881bb4ac7fe1bb35e1
Thuan <carrie.he1704@gmail.com>
2017-04-20 22:14:37 +0700

    Create domain models

---
+++ BankSystem/src/BankSystem.DAL/DomainModel/BankSystemDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BankSystem.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BankSystem.DAL.DomainModel
+{
+    public class BankSystemDbContext : IdentityDbContext<User, Role, string>, IDbContext
+    {
+        public BankSystemDbContext(DbContextOptions<BankSystemDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public BankSystemDbContext() : base()
+        {
+
+        }
+
+        public int CommitChanges()
+        {
+            return this.SaveChanges();
+        }
+
+        public new EntityEntry<TEntity> Entry<TKey, TEntity>(TEntity entity)
+            where TKey : struct
+            where TEntity : BaseEntity<TKey>
+        {
+            return this.Entry<TEntity>(entity);
+        }
+
+        public new DbSet<TEntity> Set<TKey, TEntity>()
+            where TKey : struct
+            where TEntity : BaseEntity<TKey>
+        {
+            return this.Set<TEntity>();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasMany(a => a.Accounts)
+                .WithOne(a => a.User)
+                .HasForeignKey(a => a.UserId);
+        }
+    }
+}
---
+++ BankSystem/src/BankSystem.DAL/DomainModel/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystem.DAL.DomainModel
+{
+    public class BaseEntity<TKey> where TKey: struct
+    {
+        public TKey Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
