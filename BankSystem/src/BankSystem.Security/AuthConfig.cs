5fdc7b90fba5a834b616f6c25e501adc225302a9
Thuan <carrie.he1704@gmail.com>
2017-04-20 22:14:49 +0700

    setting up security

--- BankSystem/src/BankSystem.Security/AuthConfig.cs
+++ BankSystem/src/BankSystem.Security/AuthConfig.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace BankSystem.Security
 {
-    public class Class1
+    public class AuthConfig
     {
-        public Class1()
+        public AuthConfig()
         {
         }
     }
