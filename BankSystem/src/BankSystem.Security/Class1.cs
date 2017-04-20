169412d0a36108d0d2fb830948b27e413a1c3f69
Thuan <carrie.he1704@gmail.com>
2017-04-20 21:13:11 +0700

    Init project

---
+++ BankSystem/src/BankSystem.Security/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystem.Security
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
