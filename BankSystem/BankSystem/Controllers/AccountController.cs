720840173530a76fb08dfca2b1edc822a1003ecb
nnguyen <carrie.he1704@gmail.com>
2017-04-22 18:32:09 +0700

    add account detail action

--- BankSystem/BankSystem/Controllers/AccountController.cs
+++ BankSystem/BankSystem/Controllers/AccountController.cs
@@ -14,6 +14,12 @@ namespace BankSystem.Controllers
             return View();
         }
 
+        public IActionResult AccountDetails(int? accountId)
+        {
+
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
