f65a152ea6efe2cdece75dbd466ffa131aad9600
nnguyen <carrie.he1704@gmail.com>
2017-04-23 15:44:13 +0700

    add views and ioc register

---
+++ BankSystem/BankSystem/IoCConfig/InitializeIoC.cs
@@ -0,0 +1,18 @@
+﻿using BankSystem.Service.Implementations;
+using BankSystem.Service.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystem.IoCConfig
+{
+    public class InitializeIoC
+    {
+        public static void Init(IServiceCollection services)
+        {
+            services.AddTransient<IAccountService, AccountService>();
+        }
+    }
+}
---
+++ BankSystem/BankSystem/Mapping/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BankSystem.Models.AccountViewModels;
+using BankSystem.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystem.Mapping
+{
+    public class ViewModelMapper : Profile
+    {
+        public ViewModelMapper()
+            : this("ViewModelProfile")
+        {
+        }
+        protected ViewModelMapper(string profileName)
+        : base(profileName)
+        {
+            CreateMap<AccountDto, AccountCreationVM>().ReverseMap();
+        }
+    }
+}
