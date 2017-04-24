5db7c701148255b5149fbc740dd283eefd1dd194
nnguyen <carrie.he1704@gmail.com>
2017-04-24 23:05:19 +0700

    add PagingVM.cs and update unit test pro

---
+++ BankSystem/BankSystem/Models/PagingVM.cs
@@ -0,0 +1,17 @@
+﻿using BankSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSystem.Models
+{
+    public class PagingVM<TKey, TEntity>
+    {
+        public TKey Id { get; set; }
+
+        public Pager Pager { get; set; }
+
+        public IList<TEntity> Items { get; set; }
+    }
+}
